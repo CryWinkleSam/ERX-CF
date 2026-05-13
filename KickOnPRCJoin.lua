56844565f80a3db1f4d44f1eb85fc02c7c2806e1
CryWinkleSam <uncync82@gmail.com>
2026-05-12 21:14:25 -0700

    Update KickOnPRCJoin.lua

--- KickOnPRCJoin.lua
+++ KickOnPRCJoin.lua
@@ -1,29 +1,23 @@
--- Auto kick when PRC Mod joins your server [needs ERX]
--- og
-repeat task.wait() until _G.Functions
-
-local Connections = _G.Connections
-
-Connections.OnPRCStaffJoin.Event:Connect(function(Info)
-    game:GetService("Players").LocalPlayer:Kick("!PRC Mod! joined you, Name: "..(Info.Player and Info.Player.Name or "?"))
-end)
-
--- fall back lel
-
-_G.userIDs = {16222847, 23665982, 387574627, 500161865}  -- You can add more
+_G.userIDs = {16222847, 23665982, 387574627, 500161865}
 
 local Players = game:GetService("Players")
 local lp = Players.LocalPlayer
 
+if _G.Functions and _G.Connections and _G.Connections.OnPRCStaffJoin then
+    _G.Connections.OnPRCStaffJoin.Event:Connect(function(Info)
+        local name = Info.Player and Info.Player.Name or "?"
+        lp:Kick("!PRC Mod! joined you, Name: " .. name)
+    end)
+end
+
 Players.PlayerAdded:Connect(function(plr)
-    for _, id in pairs(_G.userIDs) do
+    for _, id in ipairs(_G.userIDs) do
         if plr.UserId == id then
-            lp:Kick(plr.Name .. " - PRC Mod Joined!\n ERX")
+            lp:Kick(plr.Name .. " - PRC Mod Joined!")
             return
         end
     end
 end)
-
 
 
 -- delay option
